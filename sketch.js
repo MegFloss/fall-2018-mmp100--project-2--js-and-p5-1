f126a31bb29980fbd92a4039547419046113db77
TVHM <mcphearson.t@gmail.com>
2018-10-27 15:17:37 -0400

    modified shape

--- sketch.js
+++ sketch.js
@@ -2,9 +2,11 @@ const IMAGE_WIDTH = 600 ;
 const IMAGE_HEIGHT = 400;
 const EYE_COLOR = ('#15,#33,#37');
 const EYE_DIAMETER = 22
+
 function setup() {
 }
 
 function draw() {
-  line(50, 5, 95, 95);
+  ellipse(60, 50, 100);
+
 }
