7cacd97d2897811be9d3e542db9086eddffb7359
victornekh <victornekh@gmail.com>
2018-10-27 13:44:18 -0700

    Add the code for adding the animal name and my name.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,33 @@
-'use strict';
+
 
 const IMAGE_WIDTH = 470;
 const IMAGE_HEIGHT = 370;
 const HEAD_COLOR = "#06511E";
 
 function setup() {
+  // Setup code goes here. This function runs once.
+
+  //Create a reference to the body element.
+  let body = document.getElementsByTagName(`body`)[0];
+
+  // Create h1 and h2 elements
+  let h1 = document.createElement(`h1`);
+  let h2 = document.createElement(`h2`);
+
+  // Create text nodes for the animal and my name.
+  let animal = document.createTextNode(`Newt`);
+  let name = document.createTextNode(`Victor Nekhlyudov`);
+
+  //Add the h1 and h2 elements to the body.
+  h1.appendChild(animal);
+  h2.appendChild(name);
+  body.appendChild(h1);
+  body.appendChild(h2);
+
+  //Create the canvas for the drawing.
   createCanvas (IMAGE_WIDTH, IMAGE_HEIGHT);
   /*background (200);*/
   noFill();
-    // Setup code goes here. This function runs once.
 }
 
 function drawHead() {
@@ -36,5 +55,5 @@ function drawHead() {
 }
 
 function draw(){
-     drawHead();
+     //drawHead();
 }
