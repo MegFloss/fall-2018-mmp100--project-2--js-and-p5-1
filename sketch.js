bb67199457ca75972241cd7b1a28654188463308
carmenyeung <carmenyeung628@yahoo.com>
2018-10-27 13:29:29 -0400

    updated files

---
+++ sketch.js
@@ -0,0 +1,132 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(177,177,177);
+
+	stroke (0,0,0)
+	fill (204,0,172)
+	ellipse (320,50,40)
+
+	stroke (0,0,0)
+	fill (54,0,172)
+	ellipse (400,180,80)
+
+	stroke (0,0,0)
+	fill (204,0,172)
+	ellipse (350,260,30)
+
+  stroke (0,0,0)
+	fill (204,0,172)
+	ellipse (20,80,60)
+
+	stroke (0,0,0)
+	fill (54,0,172)
+	ellipse (80,20,40)
+
+	stroke (0,0,0)
+	fill (54,0,172)
+	ellipse (290,370,100)
+
+	stroke (0,0,0)
+	fill (54,0,172)
+	ellipse (200,330,20)
+
+	stroke (0,0,0)
+	fill (54,0,172)
+	ellipse (100,240,70)
+
+  stroke (0,0,0)
+	fill (204,0,172)
+	ellipse (80,360,40)
+
+	stroke (0,0,0)
+	fill (54,0,172)
+	ellipse (30,270,20)
+
+	beginShape();
+	stroke (0,0,0)
+	fill (0,0,0)
+	vertex(100,160);
+	vertex(80,50);
+	vertex(200,85);
+  endShape();
+
+	beginShape();
+	stroke (0,0,0)
+	fill (159,125,97)
+	vertex(120,125);
+	vertex(80,50);
+	vertex(200,85);
+  endShape();
+
+	beginShape();
+	stroke (0,0,0)
+	fill (0,0,0)
+	vertex(240,88);
+	vertex(350,55);
+	vertex(300,200);
+  endShape();
+
+	beginShape();
+	stroke (0,0,0)
+	fill (159,125,97)
+	vertex(240,88);
+	vertex(350,55);
+	vertex(290,120);
+  endShape();
+
+ 	stroke (0,0,0)
+	line (210,85,180,18)
+	line (210,85,190,18)
+	line (210,85,200,18)
+	line (210,85,210,18)
+	line (210,85,220,18)
+	line (210,85,230,18)
+	line (210,85,240,18)
+	beginShape();
+
+	stroke (0,0,0)
+	fill (159,125,97)
+	ellipse (130,340,100,60)
+	line (115,370,110,340)
+	line (130,370,130,340)
+	line (145,370,150,340)
+
+	stroke (0,0,0)
+	fill (159,125,97)
+	ellipse (290,340,100,60)
+	line (275,370,270,340)
+	line (290,370,290,340)
+	line (305,370,310,340)
+
+	stroke (0,0,0)
+	fill (159,125,97)
+	ellipse (208,190,225,215)
+	fill (0,0,0)
+	ellipse (205,258,210,80)
+
+	stroke (255,255,255)
+	fill (159,125,97)
+	ellipse (250,160,75)
+	ellipse (250,160,70)
+
+	stroke (255,255,255)
+	fill (0,0,0)
+	ellipse (150,158,40)
+	ellipse (250,160,40)
+	fill (255,255,255)
+	ellipse (155,153,10)
+	ellipse (255,155,10)
+
+	stroke (255,255,255)
+	line (170,265,230,270)
+	line (200,240,198,268)
+
+	stroke (255,255,255)
+	fill (159,125,97)
+	ellipse (200,240,40,32)
+
+
+}
