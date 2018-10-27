c341316c50935fb0c4f85a004e6596edbe2c5136
TVHM <mcphearson.t@gmail.com>
2018-10-27 16:29:11 -0400

    modified draw function

--- sketch.js
+++ sketch.js
@@ -1,17 +1,35 @@
-const IMAGE_WIDTH = 600;
-const IMAGE_HEIGHT = 400;
+const IMAGE_WIDTH = 470;
+const IMAGE_HEIGHT = 370;
 const EYE_COLOR = ('#15,#33,#37');
 const EYE_DIAMETER = 22
 
 function setup() {
+  createCanvas(IMAGE_WIDTH,IMAGE_HEIGHT);
+  background(200);
 }
 
-function draw() {
-  point(430,192);
-  line(430,192,373,154);
-  point(373,154);
-  line(373,154,295,126);
 
+function draw() {
+
+  ellipse(300,189,25)
+
+
+  beginShape()
+
+  point(420,177);
+  line(420,177,357,149);
+  line(357,149,296,126);
+  line(296,126,231,112);
+  line(231,112,159,108);
+  line(159,108,148,105);
+  point(148,105);
+  line(148,105,85,48);
+  point(85,48);
+  line(85,48,72,100);
+  line(72,100,55,128);
+  point(55,128);
+  line(55,128,5,140);
+  point()
 
 
 }
