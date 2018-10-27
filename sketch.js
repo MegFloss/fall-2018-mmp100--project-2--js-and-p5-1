2b9f0388acc0a53955059340fe29c08176cb7dcd
Raziel <raziel.ortiz@stu.bmcc.cuny.edu>
2018-10-27 16:15:14 -0400

    Progress on beak begins

--- sketch.js
+++ sketch.js
@@ -1,10 +1,31 @@
-const IMAGE_WIDTH = 2625;
-const IMAGE_HEIGHT = 1750:
+const IMAGE_WIDTH = 800;
+const IMAGE_HEIGHT = 533;
 
 function setup() {
    createCanvas(IMAGE_WIDTH, IMAGE_HEIGHT);
    background(200);
+   noLoop()
 }
 function draw(){
+  stroke(142, 0, 252);
+  line(495,187,496,179);
+
+  stroke(142, 0, 252);
+  line(496,179,493,175);
+
+  stroke(142, 0, 252);
+  line(493,175,487,171);
+
+  stroke(142, 0, 252);
+  line(487,171,459,166);
+
+  stroke(142, 0, 252);
+  line(459,166,417,153);
+
+  stroke(142, 0, 252);
+  line(417,153,411,152);
+
+  stroke(142, 0, 252);
+  line(411,152,405,147);
   
 }
