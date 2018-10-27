ecbd252d278a317a2fc9bcf5af3a13c889bec48e
victornekh <victornekh@gmail.com>
2018-10-27 13:10:28 -0700

    Set background color to grey, add noFill() to stop processing

--- sketch.js
+++ sketch.js
@@ -4,6 +4,8 @@ const IMAGE_WIDTH = 470;
 const IMAGE_HEIGHT = 370;
 function setup() {
   createCanvas (IMAGE_WIDTH, IMAGE_HEIGHT);
+  /*background (200);*/
+  noFill();
     // Setup code goes here. This function runs once.
 }
 
