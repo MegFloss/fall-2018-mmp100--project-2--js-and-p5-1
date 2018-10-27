f19f307874428d76d6e5d1ae9f471c75c12cf6be
Raziel <raziel.ortiz@stu.bmcc.cuny.edu>
2018-10-27 14:06:00 -0400

    SET up for drawing

--- sketch.js
+++ sketch.js
@@ -1,3 +1,10 @@
+const IMAGE_WIDTH = 2625;
+const IMAGE_HEIGHT = 1750:
+
 function setup() {
-   createCanvas(3000, 2000 )
+   createCanvas(IMAGE_WIDTH, IMAGE_HEIGHT);
+   background(200);
+}
+function draw(){
+  
 }
