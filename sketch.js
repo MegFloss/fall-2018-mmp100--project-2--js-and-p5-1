728ebb757402950fed87dae8d4815315b0d484bf
victornekh <victornekh@gmail.com>
2018-10-27 12:09:23 -0700

    Set canvas size

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 'use strict';
 
+const IMAGE_WIDTH = 470;
+const IMAGE_HEIGHT = 370;
 function setup() {
+  createCanvas (IMAGE_WIDTH, IMAGE_HEIGHT);
     // Setup code goes here. This function runs once.
 }
 
