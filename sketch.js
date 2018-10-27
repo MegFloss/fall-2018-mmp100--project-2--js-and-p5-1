7ee5862a3861dfac564ec3cfad527b6ac78c64c7
Rodolfo Ramiro Rivera <rodolforamirorivera@gmail.com>
2018-10-27 16:21:09 -0400

    added js code of (x,y)
    adding

---
+++ sketch.js
@@ -0,0 +1,19 @@
+const IMAGE_WIDTH = 470
+const IMAGE_HEIGHT = 370
+
+
+function setup(){
+    createCanvas(IMAGE_WIDTH, IMAGE_HEIGHT);
+    background (200)
+    noFill();
+}
+
+
+function draw(){
+
+ellipse(56, 46, 55, 55);
+
+
+
+
+}
