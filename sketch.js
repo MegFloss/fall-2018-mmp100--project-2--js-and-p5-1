26b98b00ce638a6c3e1793a2b7d20ccfa33d4944
victornekh <victornekh@gmail.com>
2018-10-29 12:09:04 -0700

    Create drawRightEyeShadow, drawRightPupil, and drawInnerPupil functions and add them to the drawRightEye function. Add the drawRightEye to the draw function so that the right eye is created.

--- sketch.js
+++ sketch.js
@@ -3,6 +3,9 @@
 const IMAGE_WIDTH = 470;
 const IMAGE_HEIGHT = 370;
 const BASE_COLOR = "#06511E";
+const PUPIL_COLOR = "#2A1A14";
+const INNER_PUPIL_COLOR = "#565455";
+const SHADOW_COLOR = "#433532";
 
 function setup() {
   // Setup code goes here. This function runs once.
@@ -55,14 +58,14 @@ function drawHead() {
 }
 
 function drawLeftEye() {
-  // Creates the head of the newt.
+  // Creates the left eye of the newt.
   /* Cancel the default black border around
   the shape created by vertex. */
   noStroke();
 
-  /*Color head*/
+  /*Color shape*/
   fill(BASE_COLOR);
-  /* Begin the shape that will be created by the five vertices
+  /* Begin the shape that will be created by the four vertices
   Using the following curves.*/
   beginShape();
   curveVertex(382, 237);
@@ -76,9 +79,81 @@ function drawLeftEye() {
   endShape();
 }
 
+function drawRightEyeShadow() {
+  //Draws right eye shadow.
+
+  /* Cancel the default black border around
+  the shape created by vertex. */
+  noStroke();
+
+  /*Color head*/
+  fill(SHADOW_COLOR);
+  /* Begin the shape that will be created by the 11 vertices
+  Using the following curves.*/
+  beginShape();
+  curveVertex(372, 278);
+  curveVertex(372, 278);
+  curveVertex(383, 274);
+  curveVertex(392, 278);
+  curveVertex(394, 283);
+  curveVertex(389, 294);
+  curveVertex(377, 297);
+  curveVertex(367, 290);
+  curveVertex(366, 282);
+  curveVertex(380, 288);
+  curveVertex(385, 282);
+  curveVertex(372, 278);
+  curveVertex(372, 278);
+
+  /* End the shape, which is required to complete the shape by the three vertices. */
+  endShape();
+}
+
+function drawRightPupil() {
+  // Creates the right pupil of the newt.
+
+  /* Cancel the default black border around
+  the shape created by vertex. */
+  noStroke();
+
+  /*Color shape*/
+  fill(PUPIL_COLOR);
+  // Begin the shape that will be created by the ellipse function
+  beginShape();
+  ellipse(383, 293, 9);
+
+  /* End the shape, which is required to complete the shape by the three vertices. */
+  endShape();
+}
+
+function drawInnerPupil() {
+  // Creates the right inner pupil of the newt.
+
+  /* Cancel the default black border around
+  the shape created by vertex. */
+  noStroke();
+
+  /*Color shape*/
+  fill(INNER_PUPIL_COLOR);
+  // Begin the shape that will be created by the ellipse function
+  beginShape();
+  ellipse(384, 295, 4);
+
+  /* End the shape, which is required to complete the shape by the three vertices. */
+  endShape();
+}
+
+function drawRightEye() {
+  // Creates the right eye of the newt.
+  drawRightEyeShadow();
+  drawRightPupil();
+  drawInnerPupil();
+}
+
 function draw(){
   //Draw the Newt's head.
      drawHead();
      drawLeftEye();
+     drawRightEye();
 
 }
